3ec20b21b737b31a3325ad67d2c0ca424a6b6728
frank-shema <shemafranko0987@gmail.com>
2025-02-04 14:28:41 +0200

    made changes

---
+++ .next/static/chunks/app_TicketPage_[id]_page_jsx_8ae0e1._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_TicketPage_[id]_page_jsx_8ae0e1._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_TicketPage_[id]_page_jsx_8ae0e1._.js",
+  "chunks": [
+    "static/chunks/_1c2026._.js"
+  ],
+  "source": "dynamic"
+});
